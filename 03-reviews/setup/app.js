ab2bd2beb85bf86726ba770f3c94e28ed2d733c7
takeMeBackpacking <jln1589@gmail.com>
2022-07-27 20:59:10 -0700

    finished project

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -37,3 +37,59 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+let currentItem = 0;
+// on document load, access item from array
+
+const showPerson = function (i) {
+  currentRecord = reviews[i];
+  console.log(currentRecord);
+  img.src = currentRecord["img"];
+  author.innerHTML = currentRecord["name"];
+  job.innerHTML = currentRecord["job"];
+  info.innerHTML = currentRecord["text"];
+
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+  console.log("page is loaded");
+  //const item = reviews[currentItem]
+  showPerson(currentItem);
+})
+
+// show next person
+nextBtn.addEventListener("click", function(){
+  currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  } 
+  showPerson(currentItem);
+});
+
+// show prev person
+prevBtn.addEventListener("click", function(){
+  currentItem--;
+  console.log(currentItem);
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  } 
+  showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener("click", function(){
+  const reviewsLength = reviews.length;
+  let myRandom = Math.floor(Math.random () * reviewsLength);
+  console.log(myRandom)
+  showPerson(myRandom);
+});
